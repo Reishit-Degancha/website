b8734a7a36d2befa6974c0a704ec4dfe621bb2c4
Eli Rosen <5184532+elirosen@users.noreply.github.com>
2026-05-25 00:24:55 +0300

    Fix: add trailing slash to base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://Reishit-Degancha.github.io',
-  base: '/website',
+  base: '/website/',
 });
